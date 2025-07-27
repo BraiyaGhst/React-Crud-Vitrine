294e474d88a6d7908449053a63f0780a3e0a2dbc
BraiyaGhst <brayan.twd@gmail.com>
2025-07-27 17:28:57 -0300

    ajustando baseURL para API Typicode

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/BraiyaGhst/fake-api-crud'
+  baseURL: "https://my-json-server.typicode.com/BraiyaGhst/fake-api-crud"
 })
-
+export async function getProducts() {
+  const response = await api.get('/products');
+  return response.data;
+}
 export default api
