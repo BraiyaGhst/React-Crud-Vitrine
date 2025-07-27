9fdd2020b66a3e76ff8a9e4d17070ad42265e74f
Brayan Escouto <110268410+BraiyaGhst@users.noreply.github.com>
2025-07-27 16:33:46 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/BraiyaGhst/fake-api-crud/'
+  baseURL: 'https://my-json-server.typicode.com/BraiyaGhst/fake-api-crud'
 })
 
 export default api
